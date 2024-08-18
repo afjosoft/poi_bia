b406323a2e920241cb343d17ea06fc297fa72058
Andres Jaramillo <afjosoft@gmail.com>
2024-08-18 15:41:16 -0500

    Bases of poi and iberia shops with zipcodes and hot_latin flag

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -10,7 +10,7 @@ from geopy.geocoders import Nominatim
 
 from arcgis.gis import GIS
 #%%
-# INGESTA DATOS ZIPCODES
+#-- INGESTA DATOS ZIPCODES
 CA_geojson = './../data/external/CA_zipcode.geojson'
 FL_geojson = './../data/external/FL_zipcode.geojson'
 IL_geojson = './../data/external/IL_zipcode.geojson'
@@ -23,12 +23,10 @@ IL_gdf = gpd.read_file(IL_geojson)
 NY_gdf = gpd.read_file(NY_geojson)
 TX_gdf = gpd.read_file(TX_geojson)
 
-hot_df = pd.read_excel('./../data/raw/hot_latin_zipcode.xlsx')
-
 zipcodes_gdf = gpd.GeoDataFrame(pd.concat([CA_gdf, FL_gdf, IL_gdf, NY_gdf, TX_gdf], ignore_index=True))
 zipcodes_gdf = zipcodes_gdf.to_crs(epsg=4326)
-#%%
-# INGESTA DATOS PUNTOS DE INTERES
+
+#-- INGESTA DATOS PUNTOS DE INTERES
 IL_df = pd.read_csv('./../data/raw/illinois.csv')
 NY_df = pd.read_csv('./../data/raw/new_york.csv')
 TX_df = pd.read_csv('./../data/raw/texas.csv')
@@ -41,34 +39,33 @@ states_df = pd.concat([IL_df, NY_df, TX_df], axis=0, ignore_index=True)
 states_df['geometry'] = states_df['geometry'].apply(loads)
 states_gdf = gpd.GeoDataFrame(states_df, geometry='geometry')
 states_gdf.set_crs(epsg=4326, inplace=True)
-#%%
-# INGESTA DATOS PRESENCIA USA
-shops_df = pd.read_excel('./../data/raw/base_clientes_iberia.xlsx')
-shops_df[['customer', 'name', 'address']] = shops_df[shops_df.columns[0]].str.split(': ', expand=True)
-shops_df['address'] = shops_df['address'].str.split(',', n=1, expand=True)[0]
-shops_df = shops_df.drop(columns=[shops_df.columns[0]])
-#%%
+
+#-- INGESTA DATOS PRESENCIA IBERIA
+shops_gdf = gpd.read_file('./../data/processed/base_clientes_iberia_consolidado.geojson')
+shops_gdf.set_crs(epsg=4326, inplace=True)
 
 #%%
+#-- ENRIQUECIMIENTO DE ZIPCODE CON HOT LATIN ZIPCODE
+hot_df = pd.read_excel('./../data/raw/hot_latin_zipcode.xlsx')
 
-# Crear una nueva columna de geometría a partir de las coordenadas
-shops_df['geometry'] = shops_df.apply(lambda row: Point(row['longitude'], row['latitude']), axis=1)
+# Convertir ZIP_CODE y zipcode a string
+zipcodes_gdf['ZIP_CODE'] = zipcodes_gdf['ZIP_CODE'].astype(str)
+hot_df['zipcode'] = hot_df['zipcode'].astype(str)
 
-# Convertir el DataFrame a un GeoDataFrame
-gdf = gpd.GeoDataFrame(shops_df, geometry='geometry')
+# Realizar el merge
+zipcodes_gdf = zipcodes_gdf.merge(hot_df[['zipcode', 'hot_latin_zipcode']], 
+                                  left_on='ZIP_CODE', 
+                                  right_on='zipcode', 
+                                  how='left')
 
-# Establecer el sistema de referencia de coordenadas (CRS)
-gdf.set_crs(epsg=4326, inplace=True)
-
-# Mostrar las primeras filas del GeoDataFrame para verificar
-print(gdf.head())
+# Eliminar la columna 'zipcode'
+zipcodes_gdf.drop(columns=['zipcode'], inplace=True)
 #%%
+#-- JOINS ESPACIALES PARA ASIGNAR ZIPCODE A CADA UBICACION
+states_gdf = gpd.sjoin(states_gdf, zipcodes_gdf[['geometry', 'ZIP_CODE', 'hot_latin_zipcode']], how='left', predicate='within')
+shops_gdf = gpd.sjoin(shops_gdf, zipcodes_gdf[['geometry', 'ZIP_CODE', 'STATE', 'hot_latin_zipcode']], how='left', predicate='within')
 
-#%%
-# Realizar el spatial join
-states_gdf = gpd.sjoin(states_gdf, zipcodes_gdf[['geometry', 'ZIP_CODE']], how='left', predicate='within')
-
-# Convertir los polígonos en centroides
+# Convertir los polígonos states_gdf en centroides
 states_gdf['geometry'] = states_gdf['geometry'].apply(lambda geom: geom.centroid if geom.type == 'Polygon' else geom)
 #%%
 
